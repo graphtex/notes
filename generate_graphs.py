b2aae5cc3bb2c0f937e413a426cd7699706e4d8b
Kevin Wu <Kevin.yuanbo@gmail.com>
2023-03-25 21:13:58 -0500

    generate

--- generate_graphs.py
+++ generate_graphs.py
@@ -14,20 +14,15 @@ plt.rcParams['savefig.dpi'] = 250
 
 IMG_LEN=924
 
-# draw graph
-DRAW_OPTIONS = {
-    "font_size": 5,
-    "font_family": 'Bradley Hand',
-    "node_size": 125,
-    # "arrowsize": 25,
-    "node_color": "white",
-    "edgecolors": "black",
-    "linewidths": 1,
-    "width": 1,
-}
 
-
-object_to_code = {}
+object_to_code = {'node':36, 
+                  'filled_node':37,
+                  'undirected_BL_TR':38,
+                  'undirected_BR_TL':39,
+                  'directed_BL_TR':40,
+                  'directed_TR_BL':41,
+                  'directed_BR_TL':42,
+                  'directed_TL_BR':43,}
 for i in range(48, 58):
     object_to_code[chr(i)] = i - 48
 for i in range(97, 123):
@@ -35,52 +30,85 @@ for i in range(97, 123):
 
 all_labels = list(string.ascii_lowercase) + [chr(num) for num in range(48, 58)]
 
-n = 3
-p = 1
 
-def gen_graph(n,p, id=0):
+
+# n = number of vertices
+# p = probability of an edge
+# f = font size
+# directed = directed/undirected graph
+# show = whether to display graph
+def gen_graph(n, p, f, directed = False, show = False, id=0):
     # create graph
-    G = nx.fast_gnp_random_graph(n, p, directed=False)
-
+    G = nx.fast_gnp_random_graph(n, p, directed=directed)
 
     # relabel nodes
     labels = dict(zip(nx.spring_layout(G).keys(), sample(all_labels, n)))
     backwards_labels = {labels[key]: key for key in labels}
     nx.relabel_nodes(G, labels, copy=False)
+    
+    # remove loops
     G.remove_edges_from(list(nx.selfloop_edges(G)))
+    
+    # adjacency matrix
     adjacency = nx.adjacency_matrix(G).toarray()
 
+    # coordinate dictionary
     pos = nx.spring_layout(G)
-    nx.draw_networkx(G, pos, **DRAW_OPTIONS)
-    # set margins for the axes so that nodes aren't clipped
+    
+    # draw settings
+    radii = list(np.arange(f + 5, f + 15))
+    sample_radii = np.random.choice(radii, n)
+    DRAW_OPTIONS = {
+        "font_size": f,
+        "font_family": 'Bradley Hand',
+        "node_size": sample_radii**2,
+        # "arrowsize": 25,
+        "node_color": "white",
+        "edgecolors": "black",
+        "linewidths": 1,
+        "width": 1,
+    }
+    
+    # draw graph
+    fig = nx.draw_networkx(G, pos, **DRAW_OPTIONS)
     ax = plt.gca()
     ax.set_aspect('equal', adjustable='box')
     plt.xlim([-2,2])
     plt.ylim([-2,2])
     plt.axis("off")
     plt.savefig(f'{id}.png', bbox_inches='tight', pad_inches=0)
+    if not show:
+        plt.close(fig)
 
+    # radii for bounding boxes
+    radius = sample_radii/100
+    letters_radius = f/100
+    
     # Generate bounding box points for training data
-    radius = [.11 for i in range(n)]
     def format_box(x1, y1, x2, y2):
         return [min(x1, x2), min(y1, y2), max(x1, x2), max(y1, y2)]
 
-    letters_radius = .05
 
     boxes = []
     training_labels = []
+    # node boxes
     for k in pos.keys():
         i = backwards_labels[k]
         boxes.append(format_box(pos[k][0] - radius[i], pos[k][1] - radius[i], pos[k][0] + radius[i], pos[k][1] + radius[i]))
-        training_labels.append(36)
+        training_labels.append(object_to_code['node'])
 
+    # label boxes
     for k in pos.keys():
         boxes.append(format_box(pos[k][0] - letters_radius, pos[k][1] - letters_radius, pos[k][0] + letters_radius, pos[k][1] + letters_radius))
         if k in object_to_code:
             training_labels.append(object_to_code[k])
 
+    # edge boxes
     for i in range(len(adjacency)):
-        for j in range(0, i + 1):
+        end = i + 1
+        if directed: 
+            end = len(adjacency[i])
+        for j in range(0, end):
             letter = labels[i]
             letter_2 = labels[j]
             if adjacency[i][j] == 1:
@@ -107,15 +135,36 @@ def gen_graph(n,p, id=0):
 
                 boxes.append(format_box(box_x1, box_y1, box_x2, box_y2))
                 
-                if slope > 0:
-                    training_labels.append(38)
-                elif slope <= 0:
-                    training_labels.append(39)
+                if not directed:
+                    if slope > 0:
+                        training_labels.append(object_to_code['undirected_BL_TR'])
+                    elif slope <= 0:
+                        training_labels.append(object_to_code['undirected_BR_TL'])
+                else:
+                    if second_above and second_right: 
+                        training_labels.append(object_to_code['directed_BL_TR'])
+                    elif second_above and not second_right:
+                        training_labels.append(object_to_code['directed_BR_TL'])
+                    elif not second_above and second_right:
+                        training_labels.append(object_to_code['directed_TL_BR'])
+                    elif not second_above and not second_right:
+                        training_labels.append(object_to_code['directed_TR_BL'])
 
     out = {"labels": torch.Tensor([training_labels]), "boxes": torch.Tensor(np.array(boxes))}
 
+#     if show:
+#         out1 = out
+#         for i in range(len(out1["boxes"])):
+#             out1["boxes"][i][0] = t.shape[2] / 2 + out1["boxes"][i][0] * t.shape[2] / 4
+#             out1["boxes"][i][2] = t.shape[2] / 2 + out1["boxes"][i][2] * t.shape[2] / 4
+#             temp = t.shape[1] / 2 - out1["boxes"][i][1] * t.shape[1] / 4
+#             out1["boxes"][i][1] = t.shape[1] / 2 - out1["boxes"][i][3] * t.shape[1] / 4
+#             out1["boxes"][i][3] = temp
+#         t_boxes = draw_bounding_boxes(t, out1["boxes"], colors = 'red', width=2)
+#         img_boxes = T.ToPILImage()(t_boxes)
+#         plt.imshow(img_boxes)
+    
     shape = [3,1,1]
-
     # Transform coordinates
     for i in range(len(out["boxes"])):
         out["boxes"][i][0] = shape[2] / 2 + out["boxes"][i][0] * shape[2] / 4
@@ -126,14 +175,11 @@ def gen_graph(n,p, id=0):
 
 
     out['boxes'] = box_convert(out['boxes'], 'xyxy', 'cxcywh')
+    
 
     rows = []
-    print(out['boxes'].shape)
-    print(out['labels'].shape)
     for l, b in zip(out['labels'][0], out['boxes']):
         # image_id,width,height,bbox,class
         rows.append([id, IMG_LEN, IMG_LEN, b, l])
     
     return rows
-
-
